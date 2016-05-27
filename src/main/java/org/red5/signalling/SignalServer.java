a6f4c9c002fb813f820335a161036a870de5bc77
dbezheckov <dima00782@gmail.com>
2016-05-27 16:55:23 +0600

    add Login

--- src/main/java/org/red5/signalling/SignalServer.java
+++ src/main/java/org/red5/signalling/SignalServer.java
@@ -5,14 +5,13 @@ import org.json.JSONObject;
 import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
 import java.io.IOException;
-
+import java.util.HashMap;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-
 
 /**
  * 
@@ -21,35 +20,78 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/ws")
 public class SignalServer {
-	private static final Logger log = Red5LoggerFactory.getLogger(SignalServer.class, "signalling");
+	
+	private static final Logger LOG = Red5LoggerFactory.getLogger(SignalServer.class, "signalling");
+	private static final JSONObject LOGIN_SUCCESS_MESSAGE;
+	private static final JSONObject LOGIN_FAIL_MESSAGE;
+	
+	private final HashMap<String, Session> users = new HashMap<String, Session>();
+	
+	static
+	{
+		JSONObject loginTrueMessage = null;
+		JSONObject loginFalseMessage = null;
+		
+		try {
+			loginTrueMessage = new JSONObject("{ \"TYPE\" : \"Login\", \"SUCCESS\" : \"true\"}");
+			loginFalseMessage = new JSONObject("{ \"TYPE\" : \"Login\", \"SUCCESS\" : \"false\"}");
+		} catch (JSONException e) {
+			LOG.error("json constant haven't constructed " + e.getMessage());
+		}
+		
+		LOGIN_SUCCESS_MESSAGE = loginTrueMessage;
+		LOGIN_FAIL_MESSAGE = loginFalseMessage;
+	}
 	
     @OnOpen
     public void onOpen(Session session) throws IOException {
-    	log.info("User connected");
+    	LOG.info("User connected");
         session.getBasicRemote().sendText("Ok, you are connected");
     }
 
     @OnMessage
-    public String onMessage(String message) {
-    	log.info("Got message: " + message);
+    public void onMessage(Session session, String message, boolean last) throws IOException {
+    	LOG.info("Got message: " + message);
+    	
+    	JSONObject data = null;
+    	String type = null;
+    	String id = null;
     	
     	try {
-			JSONObject data = new JSONObject(message);
-			log.info("Id: " + data.get("ID"));
+    		data = new JSONObject(message);
+    		id = data.getString("ID");
+    		type = data.getString("TYPE");
+			LOG.info("Id: " + id);
 		} catch (JSONException e) {
-			log.error("Error parsing JSON " + e.getMessage());
+			LOG.error("Error parsing JSON " + e.getMessage());
 		}
     	
-        return message + " (from your server)";
+    	switch (type) {
+    	case "Login": {
+    		LOG.info("User logged in as " + id);
+    		if (users.containsKey(id)) {
+    			session.getBasicRemote().sendText(LOGIN_FAIL_MESSAGE.toString(), last);
+    		}
+    		else {
+    			users.put(id, session);
+    			session.getBasicRemote().sendText(LOGIN_SUCCESS_MESSAGE.toString(), last);
+    		}
+    		break;
+    	}
+    	default:
+    		session.getBasicRemote().sendText(
+    				"{ \"TYPE\" : \"Error\", \"Message\" : \"Unrecognized command:" + type +  "\"}", last
+    		);
+    	}
     }
 
     @OnError
     public void onError(Throwable t) {
-    	log.error("Error " + t.getMessage());
+    	LOG.error("Error " + t.getMessage());
     }
 
     @OnClose
     public void onClose(Session session) {
-    	log.info("User disconnected");
+    	LOG.info("User disconnected");
     }
 }
